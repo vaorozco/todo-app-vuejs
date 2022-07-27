06f36cd1723678ec9a5a78a925329c1b09581a96
Víctor Alonso Orozco Calderón <vaorozco@hotmail.com>
2022-07-27 11:41:09 -0600

    app ready

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -1,26 +1,126 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+<script setup>
+import { ref, onMounted, computed, watch } from "vue";
+const todos = ref([]);
+const name = ref("");
+const input_content = ref("");
+const input_category = ref(null);
+const todos_asc = computed(() =>
+  // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+  todos.value.sort((a, b) => {
+    return b.createdAt - a.createdAt;
+  })
+);
+watch(name, (newVal) => {
+  localStorage.setItem("name", newVal);
+});
+watch(
+  todos,
+  (newVal) => {
+    localStorage.setItem("todos", JSON.stringify(newVal));
+  },
+  {
+    deep: true,
   }
-}
+);
+const addTodo = () => {
+  if (input_content.value.trim() === "" || input_category.value === null) {
+    return;
+  }
+  todos.value.push({
+    content: input_content.value,
+    category: input_category.value,
+    done: false,
+    editable: false,
+    createdAt: new Date().getTime(),
+  });
+
+  input_content.value = '';
+  input_category.value = null;
+
+};
+const removeTodo = (todo) => {
+  todos.value = todos.value.filter((t) => t !== todo);
+};
+
+onMounted(() => {
+  name.value = localStorage.getItem("name") || "";
+  todos.value = JSON.parse(localStorage.getItem("todos")) || [];
+});
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <main class="app">
+    <section class="greeting">
+      <h2 class="title">
+        Welcome back,
+        <input type="text" id="name" placeholder="Name here" v-model="name" />
+      </h2>
+    </section>
+    <section class="create-todo">
+      <h3>CREATE A TASK</h3>
+
+      <form id="new-todo-form" @submit.prevent="addTodo" autocomplete="off">
+        <h4>What's on your todo list?</h4>
+        <input
+          type="text"
+          name="content"
+          id="content"
+          placeholder="e.g. make a video"
+          v-model="input_content"
+        />
+
+        <h4>Choose a category</h4>
+        <div class="options">
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category1"
+              value="business"
+              v-model="input_category"
+            />
+            <span class="bubble business"></span>
+            <div>Business</div>
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="category"
+              id="category2"
+              value="personal"
+              v-model="input_category"
+            />
+            <span class="bubble personal"></span>
+            <div>Personal</div>
+          </label>
+        </div>
+
+        <input type="submit" value="Add todo" />
+      </form>
+    </section>
+    <section class="todo-list">
+      <h3>TODO LIST</h3>
+      <div class="list" id="todo-list">
+        <div v-for="(todo,index) in todos_asc" :key="index" :class="`todo-item ${todo.done && 'done'}`">
+          <label>
+            <input type="checkbox" v-model="todo.done" />
+            <span
+              :class="`bubble ${
+                todo.category == 'business' ? 'business' : 'personal'
+              }`"
+            ></span>
+          </label>
+
+          <div class="todo-content">
+            <input type="text" v-model="todo.content" />
+          </div>
+
+          <div class="actions">
+            <button class="delete" @click="removeTodo(todo)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './main.css' 
 
 createApp(App).mount('#app')
+//TODO:
+//FIXME:
+
+
